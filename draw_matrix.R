1883f55d1512fa3d11352b39319789cae8ce2e42
jgalgarra <ghostdatalearner@ghostmail.com>
2015-06-12 16:17:19 +0200

    kcore division lines in interaction matrix

--- draw_matrix.R
+++ draw_matrix.R
@@ -6,13 +6,46 @@ library("reshape2")
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 source("network-kanalysis.R")
-network_name <- "M_SD_016"
-result_analysis <- analyze_network(paste0(network_name,".csv"), directory = "data/", guild_a = "Plant", guild_b = "Pollinator", plot_graphs = TRUE)
-
-
 orderbykcorenumkdegree <- TRUE
 put_title <- TRUE
+lsize_axis <- 12
 
+network_name <- "M_SD_013"
+result_analysis <- analyze_network(paste0(network_name,".csv"), directory = "data/", guild_a = "Plant", guild_b = "Pollinator", plot_graphs = TRUE)
+
+changes_kcore <- function(kcorevector)
+{
+  auxdf <- data.frame(c())
+  kcoreact <- kcorevector[1]
+  for (i in 2:length(kcorevector))
+    if (kcoreact != kcorevector[i]){
+      auxdf <- rbind(auxdf,c(kcoreact,i-1))
+      kcoreact = kcorevector[i]
+    }
+  auxdf <- rbind(auxdf,c(kcoreact,i))
+  names(auxdf) <- c("kcorenum","species")
+  return(auxdf)
+}
+
+draw_kline <- function(p,kcorenum,xpos,ylim,ptext,vertical = TRUE, pcolor="grey")
+{
+  if (vertical) {
+  x <- c(xpos+0.5,xpos+0.5)
+  y <- c(-0.25,ylim+0.5)
+  }
+  else{
+  y <- c(xpos+0.5,xpos+0.5)
+  x <- c(0,ylim+0.5)
+  }
+    
+  ds <- data.frame(x,y)
+  p <- p + geom_line(data=ds,aes(x=x,y=y),color=pcolor,linetype="dashed")
+  if (vertical)
+      p <- p+ geom_text(data=NULL,x=ptext,y=0.5+0.5*as.integer(ylim>10),vjust=1,label=paste0(kcorenum,"k"), cex=3, color = pcolor,  fontface="italic")
+  else
+    p <- p+ geom_text(data=NULL,y=ptext,x=ylim+0.2,hjust=0.5,vjust=.5,label=paste0(kcorenum,"k"), angle=270, cex=3, color = pcolor, fontface="italic")
+  return(p)
+}
 
 interaction_mat <- as.data.frame(result_analysis$matrix)
 dropchars <- "[\\.,-]"
@@ -83,56 +116,94 @@ for (i in 1:nrow(interaction_mat.s))
   interaction_mat.s$vcols_b[i]<- vcols_b[interaction_mat.s$kcorenum_b[i]]
 }
 
-if (nrow(interaction_mat.s)<ncol(interaction_mat.s)){
+if (nrow(interaction_mat)<ncol(interaction_mat)){
   if (orderbykcorenumkdegree){
     interaction_mat.s$Name <- factor(interaction_mat.s$Name,
-                                     levels=interaction_mat.s$Name[rev(order(interaction_mat.s$kcorenum_a,
-                                                                             interaction_mat.s$kdegree_a))])
+                                     levels=interaction_mat.s$Name[order(interaction_mat.s$kcorenum_b,
+                                                                             interaction_mat.s$kdegree_b)])
     interaction_mat.s$variable <- factor(interaction_mat.s$variable,
-                                         levels=interaction_mat.s$variable[order(interaction_mat.s$kcorenum_b,
-                                                                                 interaction_mat.s$kdegree_b)])
+                                         levels=interaction_mat.s$variable[rev(order(interaction_mat.s$kcorenum_a,
+                                                                                 interaction_mat.s$kdegree_a))])
   }
   vec_colors_y <- interaction_mat.s$vcols_b
   vec_colors_x <- interaction_mat.s$vcols_a
-  col_text_y <- "firebrick2"
-  col_text_x <- "slateblue2"
+  col_text_y <- "tomato3"
+  col_text_x <- "cadetblue4"
   p <- ggplot(interaction_mat.s, aes(variable,Name))
+  species_axis_y <- rev(unique(levels(interaction_mat.s$Name)))
+  kcorenum_axis_y <- c()
+  for (i in 1:length(species_axis_y)){
+    kcorenum <- interaction_mat.s[interaction_mat.s$Name == species_axis_y[i],]$kcorenum_b[1]
+    kcorenum_axis_y <- c(kcorenum_axis_y,kcorenum) 
+  }
+  species_axis_x <- unique(levels(interaction_mat.s$variable))
+  kcorenum_axis_x <- c()
+  for (i in 1:length(species_axis_x)){
+    kcorenum <- interaction_mat.s[interaction_mat.s$variable == species_axis_x[i],]$kcorenum_a[1]
+    kcorenum_axis_x <- c(kcorenum_axis_x,kcorenum) 
+  }
+  pcolory="sienna1"
+  pcolorx="cadetblue3"
   
 } else {
   if (orderbykcorenumkdegree){
     interaction_mat.s$Name <- factor(interaction_mat.s$Name,
                                      levels=interaction_mat.s$Name[rev(order(interaction_mat.s$kcorenum_b,
-                                                                             interaction_mat.s$kdegree_b))])
+                                                                         interaction_mat.s$kdegree_b))])
     interaction_mat.s$variable <- factor(interaction_mat.s$variable,
-                                     levels=interaction_mat.s$variable[order(interaction_mat.s$kcorenum_a,
-                                                                       interaction_mat.s$kdegree_a)])
+                                         levels=interaction_mat.s$variable[order(interaction_mat.s$kcorenum_a,
+                                                                                     interaction_mat.s$kdegree_a)])
   }
-#   if (orderbykdegree){
-#     interaction_mat.s$Name <- factor(interaction_mat.s$Name,levels=interaction_mat.s$Name[rev(order(interaction_mat.s$kdegree_b))])
-#     interaction_mat.s$variable <- factor(interaction_mat.s$variable,levels=interaction_mat.s$variable[order(interaction_mat.s$kdegree_a)])
-#   }
   vec_colors_y <- interaction_mat.s$vcols_a
   vec_colors_x <- interaction_mat.s$vcols_b
-  col_text_x <- "firebrick2"
-  col_text_y <- "slateblue2"
+  col_text_x <- "tomato3"
+  col_text_y <- "cadetblue4"
   p <- ggplot(interaction_mat.s, aes(Name,variable))
   
+  species_axis_y <- rev(unique(levels(interaction_mat.s$variable)))
+  kcorenum_axis_y <- c()
+  for (i in 1:length(species_axis_y)){
+    kcorenum <- interaction_mat.s[interaction_mat.s$variable == species_axis_y[i],]$kcorenum_a[1]
+    kcorenum_axis_y <- c(kcorenum_axis_y,kcorenum) 
+  }
   
+  species_axis_x <- unique(levels(interaction_mat.s$Name))
+  kcorenum_axis_x <- c()
+  for (i in 1:length(species_axis_x)){
+    kcorenum <- interaction_mat.s[interaction_mat.s$Name == species_axis_x[i],]$kcorenum_b[1]
+    kcorenum_axis_x <- c(kcorenum_axis_x,kcorenum) 
+  }
+  pcolorx="sienna1"
+  pcolory="cadetblue3"
 }
 
+changes_x <- changes_kcore(kcorenum_axis_x)
+changes_y <- changes_kcore(kcorenum_axis_y)
+changes_y$kcorenum <- rev(changes_y$kcorenum)
 
 p<- p +
   geom_tile(aes(fill = kcoremeasure), colour = "white") + 
   scale_fill_gradient(low = "white", high = "darkorchid4") + 
   scale_x_discrete("", expand = c(0, 0)) + 
   scale_y_discrete("", expand = c(0, 0)) + 
-  theme_bw(base_size = 10) + 
+  theme_bw(base_size = lsize_axis) + 
   theme(legend.position = "none",
         axis.ticks = element_blank(), 
         axis.text.y = element_text(angle = 0 , hjust = 0, face="bold", color = col_text_y),
-        axis.text.x = element_text(angle = 270 , hjust= 0, vjust = 1,  face="bold" , color = col_text_x )
+        axis.text.x = element_text(angle = 270 , hjust= 0, vjust = 0.5,  face="bold" , color = col_text_x ),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_blank()
         )
+anterior <- 1
+for (i in 1:nrow(changes_x)){
+  p <- draw_kline(p,changes_x$kcorenum[i],changes_x$species[i], length(species_axis_y), 0.5+(changes_x$species[i]+anterior)/2 ,pcolor=pcolorx)
+  anterior <- changes_x$species[i]
+}
 
+for (i in 1:nrow(changes_y))
+  p <- draw_kline(p,changes_y$kcorenum[i],changes_y$species[i], length(species_axis_x) , changes_y$species[i]-0.1,vertical = FALSE, pcolor=pcolory)
 
 if (put_title)
   p <- p + ggtitle(sprintf("%s NODF: %.02f Modularity: %.04f Average K-distance: %.02f\n", network_name, NODF, Modularity, MeanKdistance))
